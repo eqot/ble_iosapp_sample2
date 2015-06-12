21f749741a4e6087f9decc503930a0e0ceb20945
Ikuo Terado <eqobar@gmail.com>
2015-06-13 01:50:20 +0900

    Polish 2nd view in settings

--- js/SettingsTab.js
+++ js/SettingsTab.js
@@ -3,6 +3,7 @@
 var React = require('react-native');
 var {
   View,
+  SwitchIOS,
   Text,
   TouchableHighlight,
   ListView,
@@ -40,10 +41,10 @@ var SettingView = React.createClass({
 
   pressRow(id) {
     this.props.navigator.push({
-      component: SettingView,
+      component: SettingDetailView,
       title: this.props.settings[id].name,
       passProps: {
-        settings: this.props.settings
+        settings: this.props.settings[id]
       },
     });
   },
@@ -67,6 +68,56 @@ var SettingView = React.createClass({
           <Text style={styles.arrow}> > </Text>
         </View>
       </TouchableHighlight>
+    );
+  }
+});
+
+var SettingDetailView = React.createClass({
+  getInitialState() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var params = [
+      {
+        name: 'Color',
+        items: ['#0000ff', '#00ff00', '#ff0000'],
+      },
+      {
+        name: 'Pattern',
+        items: ['Standard', 'Quick', 'Slow'],
+      },
+      {
+        name: 'Vibrator',
+        items: true,
+      },
+    ];
+
+    return {
+      dataSource: ds.cloneWithRows(params),
+    };
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
+      </View>
+    );
+  },
+
+  renderRow(rowData, sectionId: number, rowId: number) {
+    var item = null;
+    if (Array.isArray(rowData.items)) {
+      item = <Text>aaa</Text>
+    } else if (typeof(rowData.items) === 'boolean') {
+      item = <SwitchIOS />
+    }
+    return (
+      <View style={styles.row}>
+        <Text>{rowData.name}</Text>
+        {item}
+      </View>
     );
   }
 });
